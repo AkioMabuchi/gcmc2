75814f773aa536475ed273c809f33361cb521b3d
AkioMabuchi <mabuchiakio@gmail.com>
2021-01-23 16:48:55 +0900

    update

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def top
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def contact_form
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get top" do
+    get home_top_url
+    assert_response :success
+  end
+
+  test "should get terms" do
+    get home_terms_url
+    assert_response :success
+  end
+
+  test "should get privacy" do
+    get home_privacy_url
+    assert_response :success
+  end
+
+  test "should get contact_form" do
+    get home_contact_form_url
+    assert_response :success
+  end
+
+end
