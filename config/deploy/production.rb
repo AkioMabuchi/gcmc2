8ccfccd2a1699cbccca0233893f0cfd5010d06fd
AkioMabuchi <mabuchiakio@gmail.com>
2021-04-11 00:03:09 +0900

    fixed bug

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "dualstack.gcmc-515670996.ap-northeast-1.elb.amazonaws.com.", user: "akio", roles: %w{app db web}
+server "gcmatchingcenter.com", user: "akio", roles: %w{app db web}
 
 set :ssh_options, keys: "~/.ssh/gcmc_key_rsa"
