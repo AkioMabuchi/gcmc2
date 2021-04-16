753e00cbdf093b2db1d4fce02973f04b4b6fac6b
AkioMabuchi <mabuchiakio@gmail.com>
2021-04-16 11:22:44 +0900

    changed devise path

--- config/routes.rb
+++ config/routes.rb
@@ -4,30 +4,38 @@ Rails.application.routes.draw do
   get "terms", to: "home#terms", as: "terms"
   get "privacy", to: "home#privacy", as: "privacy"
 
-  devise_for :users,
+  devise_for :users, skip: [:sessions, :registrations],
              path: "",
-             path_names: {
-                 sign_up: "/signup",
-                 sign_in: "/login",
-                 edit: "/settings"
-             },
-             controllers: {
-                 registrations: "users/registrations",
-                 sessions: "users/sessions",
-                 omniauth_callbacks: "users/omniauth_callbacks"
+             controllers:{
+               confirmations: "users/confirmations",
+               passwords: "users/passwords",
+               registrations: "users/registrations",
+               sessions: "users/sessions",
+               omniauth_callbacks: "users/omniauth_callbacks"
              }
-
   devise_scope :user do
-    get "settings/:mode", to: "users/registrations#edit", constraints: {mode: %w[email password notification sns destroy]}
+    get "login", to: "users/sessions#new", as: "new_user_session"
+    post "login", to: "users/sessions#create", as: "user_session"
+    delete "logout", to: "users/sessions#destroy", as: "destroy_user_session"
+    get "signup", to: "users/registrations#new", as: "new_user_registration"
+    post "signup", to: "users/registrations#create", as: "create_user_registration"
+    get "settings", to: "users/registrations#edit", as: "edit_user_registration"
+    patch "settings", to: "users/registrations#update", as: "user_registration"
+    get "settings/:mode", to: "users/registrations#edit", constraints: { mode: %w[email password notification sns destroy] }
     get "settings/email", as: "edit_email_user_registration"
+    patch "settings/email", to: "users/registrations#update", as: "email_user_registration"
     get "settings/password", as: "edit_password_user_registration"
+    patch "settings/password", to: "users/registrations#update", as: "password_user_registration"
     get "settings/notification", as: "edit_notification_user_registration"
+    patch "settings/notification", to: "users/registrations#update", as: "notification_user_registration"
     get "settings/sns", as: "edit_sns_user_registration"
+    patch "settings/sns", to: "users/registrations#update", as: "sns_user_registration"
     get "settings/destroy", as: "edit_destroy_user_registration"
+    delete "settings/destroy", to: "users/registrations#destroy", as: "destroy_user_registration"
   end
 
   resources :users, only: [:index, :show]
-  resources :teams, except: [:new], path_names: {edit: :settings} do
+  resources :teams, except: [:new], path_names: { edit: :settings } do
     member do
       get :tags_edit, path: "settings/tags"
       get :environments_edit, path: "settings/environments"
@@ -38,7 +46,6 @@ Rails.application.routes.draw do
       patch :environments_update, path: "environments"
     end
   end
-
 
   resource :twitters, only: [:destroy], path: "disconnect/twitter"
   resource :githubs, only: [:destroy], path: "disconnect/github"
